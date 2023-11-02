11dcac44f63fa2e37b2c593f009d726ed2b56d89
Ainulalam <ainulalam413@gmail.com>
2023-11-02 19:30:56 +0530

    Maximum Sum Array Using Prefix Sum

--- MaxSubArray.java
+++ MaxSubArray.java
@@ -2,18 +2,27 @@ public class MaxSubArray {
 
     public static void MaxSub(int arr[]){
         int maxsub=Integer.MIN_VALUE;
+        int prefix []=new int[arr.length];
+        int currentSum=0,start,end;
+
+        // Calculate Pefrix
+        for(int i= 1;i<prefix.length;i++){
+            prefix[i]=prefix[i-1]+arr[i];
+        }
         
         for(int i=0;i<arr.length;i++){
+            start=i;
             for(int j=i;j<arr.length;j++){
-                int currentSum=0;
-                for(int k=i;k<=j;k++){
-                    // Sum of SUB aray
-                    currentSum +=arr[k];
-                }
-                if(maxsub<currentSum){
+                // Prefix method
+                end=j;
+                currentSum=start==0?prefix[end]:prefix[end]-prefix[start-1];
+                
+                // MaxArray
+                if(currentSum>maxsub){
                     maxsub=currentSum;
                 }
-                System.out.println("Curent Max Sub Array "+ currentSum);   // Current Max 
+                
+
             }
         }
         System.out.println(" Maximum Sub array is "+ maxsub);    // Maximum of Sub Array
